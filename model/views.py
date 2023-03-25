7b03e20b24d1980a09d80db3990d88f95173784a
GiHwan2 <sandwich5011@naver.com>
2023-03-25 18:05:24 +0900

    fix : 버그 수정

--- model/views.py
+++ model/views.py
@@ -5,8 +5,6 @@ from .tasks import descison
 import numpy as np
 import cv2
 import requests
-
-
 class GarbageDecisionAPI(APIView):
     
     def get(self):
@@ -14,19 +12,18 @@ class GarbageDecisionAPI(APIView):
     
     
     def post(self, request):
-        file = request.FILES['id'].read()
+        file = request.FILES['file']
         act_no = request.POST.get('act_no')
-        file_bytes = np.fromstring(file, np.uint8)
+        file_bytes = np.fromstring(file.read(), np.uint8)
         image = cv2.imdecode(file_bytes,cv2.IMREAD_COLOR)
         image = cv2.resize(image,(224,224))
         result = descison.delay(image.tolist())
         
-        form_data = {
-            'file': file,
+        up = {'file':(file.name, file, "multipart/form-data")}
+        data = {
             'category': result.get(),
             'act_no': act_no
         }
-        server = requests.post('http://localhost:8080/api/v1/gcs',data=form_data)
-        
+        server = requests.post('http://localhost:8080/api/v1/gcs',files=up,data=data)
         
         return Response(server.text,status=200)
